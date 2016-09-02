ceb9d0f337d98b88e8b1915077fcf7cc8f980512
pitronalldak <ryabinin.p.b@gmail.com>
2016-09-02 13:08:30 +0300

    add webpack.production.config.js

---
+++ webpack.production.config.js
@@ -0,0 +1,52 @@
+/* eslint strict: 0 */
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: true,
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.BROWSER': true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style', 'css', 'postcss']
+    },
+    {
+      test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg(\?v\=.*)?$|\.woff(\?v\=.*)?$|\.ttf(\?v\=.*)?$|\.eot(\?v\=.*)?$|\.woff?2(\?v\=.*)?/, // eslint-disable-line max-len
+      loader: 'file-loader?name=[path][name].[ext]'
+    }]
+  },
+  postcss() {
+    return [precss, autoprefixer];
+  },
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  }
+};
