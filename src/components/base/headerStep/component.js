edc66b0f99f6c4eb251cf93eae34d9adc100ee7b
pitronalldak <ryabinin.p.b@gmail.com>
2016-09-24 18:27:03 +0300

    task_46 fixed line on step_menu

--- src/components/base/headerStep/component.js
+++ src/components/base/headerStep/component.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 const HeaderStepsComponent = (props) => {
-
-  const renderStep = (step, label) => (
+  const handleLink = (event) => {
+    event.preventDefault();
+  };
+  const renderStep = (step, label, handler) => (
     <li className={step < props.currenStep ? 'prev' : step === props.currenStep ? 'active' : null}>
-      <span>
-        <i className="ico-check" />
-      </span>
-      {label}
+      <a href="" onClick={handler}>
+        <span>
+          <i className="ico-check" />
+        </span>
+        {label}
+      </a>
     </li>
   );
   return (
     <header className="section-head">
       <ul className="list-links">
-        {renderStep(1, 'Online I.D')}
-        {renderStep(2, 'Account Creation')}
-        {renderStep(3, 'Employment Verification')}
-        {renderStep(4, 'Coverage Selection')}
-        {renderStep(5, 'Self Essessment')}
+        {renderStep(1, 'Online I.D', handleLink)}
+        {renderStep(2, 'Account Creation', handleLink)}
+        {renderStep(3, 'Employment Verification', handleLink)}
+        {renderStep(4, 'Coverage Selection', handleLink)}
+        {renderStep(5, 'Self Essessment', handleLink)}
       </ul>
     </header>
   );
